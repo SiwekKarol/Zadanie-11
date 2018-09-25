38e60b7b8cbdac30dd6e145fc7c014e3d7a2de79
KarolSiwek <siwek.karol1992@gmail.com>
2018-09-25 10:48:57 +0200

    Zadanie z układem współrzędnych

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj x: ");
+        int x = scan.nextInt();
+        System.out.println("Podaj y: ");
+        int y = scan.nextInt();
+        scan.close();
+        if (x > 0 && y > 0) {
+            System.out.println("Punkt który podałeś leży w I ćwiartce");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Punkt który podałeś leży w II ćwiartce");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Punkt który podałeś leży w III ćwiartce");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Punkt który podałeś leży w IV ćwiartce");
+        } else if (x == 0 && y == 0) {
+            System.out.println("Punkt który podałeś jest początkiem układu współrzędnych");
+        }
+    }
+}
